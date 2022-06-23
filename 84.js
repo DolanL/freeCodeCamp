530da5205fd475cb7dff54f8a738420584ac8c53
Dolan Lakshinov <dlakshinov@inbox.ru>
2022-06-24 02:36:22 +0300

    commit

---
+++ 84.js
@@ -0,0 +1,8 @@
+const myDog = {
+    // Only change code below this line
+    "name": "archi",
+    legs: 4,
+    "tails": 1,
+    "friends": ["Hatiko", "Tuzik"]
+    // Only change code above this line
+  };
---
+++ 93.js
@@ -0,0 +1,15 @@
+const myMusic = [
+    {
+      "artist": "Billy Joel",
+      "title": "Piano Man",
+      "release_year": 1973,
+      "formats": [
+        "CD",
+        "8T",
+        "LP"
+      ],
+      "gold": true
+    }
+  ];
+  
+  myMusic.push({"artist": "Dolan", "title": "Guitar Man", "release_year" : 2013, "formats": ["CD", "8T"]});
